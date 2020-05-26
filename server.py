ac4c56a298fdfe3252097206b39b14d5ec4b86f0
Gavin <gavinbrrtt@gmail.com>
2020-05-26 15:27:36 -0700

    updated server

--- server.py
+++ server.py
@@ -1,21 +1,24 @@
-import os
-import json
-from flask import Flask, flash, request, redirect, url_for, render_template, jsonify, send_file
+import base64
+from src.steg import encode
+from flask import Flask, request, render_template, jsonify
 
 app = Flask(__name__)
 
-@app.route('/upload', methods=['POST'])
+@app.route('/upload', methods=['POST', 'GET'])
 def encode_image():
-	f = open("newfile.png", "wb")
 	r = request.files['file']
 	# read the image binary
 	data = r.read()
-	print(data)
-	f.write(data)
-	f.close()
-	return jsonify({"data":"received"})
+	# encode the image with a message
+	enc_data = encode(data, "I'm Gavin")
+	# encode image as base64
+	serialized_data = base64.b64encode(enc_data).decode("UTF-8")
+	# add image/png heading
+	serialized = f"data:image/png;base64,{str(serialized_data)}"
+	# return the serialized data in a json object
+	return jsonify({"enc":serialized})
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def main():
     return render_template('./index.html') 
 
